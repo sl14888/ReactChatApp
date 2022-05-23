7750d710b5b872fc15864823ef9598ee6e9313ff
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-05-24 00:54:34 +0300

    реализовал компонент шапки

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { CHAT_ROUTE, LOGIN_ROUTE } from '../utils/consts';
 import { privateRoutes, publicRoutes } from '../utils/routes';
 
 const AppRouter = () => {
   const user = false;
+  // Проверка входа
   return user ? (
     <Routes>
       {privateRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} element={Component} exact />
       ))}
+      {/* Редирект в новой версии роутера */}
       <Route path="*" element={<Navigate to="/chat" replace />} />
     </Routes>
   ) : (
@@ -17,6 +18,7 @@ const AppRouter = () => {
       {publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} element={Component} exact />
       ))}
+      {/* Редирект в новой версии роутера */}
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,182 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import PersonIcon from '@mui/icons-material/Person';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Button } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../utils/consts';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
+
+const settings = ['Профиль', 'Выйти'];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const user = false;
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="primary" position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <ChatBubbleIcon
+              sx={{ display: { xs: 'none', md: 'flex', color: '#66b2ff' }, mr: 1 }}
+            />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                color: '#66b2ff',
+                textDecoration: 'none',
+              }}
+            >
+              CHATAPP KUBSU
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              ></Menu>
+            </Box>
+            <ChatBubbleIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
+            {user ? (
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Открыть">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      sx={{ bgcolor: 'white' }}
+                      alt="ava"
+                      src="/static/images/avatar/2.jpg"
+                    >
+                      <PersonIcon />
+                    </Avatar>
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            ) : (
+              <Box sx={{ flexGrow: 0 }}>
+                <NavLink to={LOGIN_ROUTE}>
+                  <Button
+                    variant={'contained'}
+                    sx={{ color: 'white', bgcolor: '66b2ff' }}
+                  >
+                    Логин
+                  </Button>
+                </NavLink>
+              </Box>
+            )}
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
 };
 
 export default Navbar;
