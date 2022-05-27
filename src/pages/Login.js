5b4f7f71f671bd25434d3d43dc926a3f1c32ecf9
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-05-28 01:15:09 +0300

    создал компонент логина

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useContext, useState } from 'react';
+
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
@@ -7,9 +8,16 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import GoogleIcon from '@mui/icons-material/Google';
-import { NavLink } from 'react-router-dom';
-import { REG_ROUTE } from '../utils/consts';
 import { Grid } from '@mui/material';
+
+import { NavLink, useNavigate } from 'react-router-dom';
+import { REG_ROUTE } from '../utils/consts';
+
+import { Context } from '../firebase';
+
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { updateDoc, doc } from 'firebase/firestore';
+import firebase from 'firebase/compat/app';
 
 const theme = createTheme({
   palette: {
@@ -21,13 +29,54 @@ const theme = createTheme({
 });
 
 const Login = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  // получаю данные через контекст
+  const { auth, firestore } = useContext(Context);
+
+  const history = useNavigate();
+
+  const signIn = async () => {
+    //Авторизация по google через popup
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const { user } = await auth.signInWithPopup(provider);
+    console.log(user);
+  };
+  // данные для регистрации
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+    error: null,
+    loading: false,
+  });
+  // достаю из даты данные через деструктуризацию
+  const { email, password, error, loading } = data;
+  // делаю инпут управляемым
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  // асинхронная функция при нажатии на кнопку
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setData({ ...data, error: null, loading: true });
+    // валидация на пустоту
+    if (!email || !password) {
+      setData({ ...data, error: 'Заполнить все поля' });
+    }
+
+    try {
+      // регистрация по почте
+      const result = await signInWithEmailAndPassword(auth, email, password);
+
+      // создаю колекцию в дб
+      await updateDoc(doc(firestore, 'users', result.user.uid), {
+        isOnline: true,
+      });
+      // очещаю поля
+      setData({ email: '', password: '', error: null, loading: false });
+      history('/');
+    } catch (error) {
+      // вывожу ошибку
+      setData({ ...data, error: error.message, loading: false });
+    }
   };
 
   return (
@@ -49,6 +98,8 @@ const Login = () => {
               margin="normal"
               required
               fullWidth
+              onChange={handleChange}
+              value={email}
               id="email"
               label="Почта"
               name="email"
@@ -59,17 +110,31 @@ const Login = () => {
               margin="normal"
               required
               fullWidth
+              onChange={handleChange}
+              value={password}
               name="password"
               label="Пароль"
               type="password"
               id="password"
             />
+            {/* вывод ошибки */}
+            {error ? (
+              <Typography align="center" color="error">
+                {error}
+              </Typography>
+            ) : null}
 
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-              Войти
+            <Button
+              disabled={loading}
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              {loading ? 'Загрузка...' : 'Войти'}
             </Button>
-            <Button fullWidth variant="contained" sx={{ mb: 2 }}>
-              Войти через Google
+            <Button disabled={loading} fullWidth variant="contained" sx={{ mb: 2 }}>
+              {loading ? 'Загрузка...' : 'Войти через Google'}
               <GoogleIcon />
             </Button>
             <Grid container justifyContent="flex-end" sx={{ mt: 2 }}>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react';
+// MUI-UI
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
@@ -8,15 +9,15 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-import { NavLink } from 'react-router-dom';
+//Реакт роутер
+import { NavLink, useNavigate } from 'react-router-dom';
 import { LOGIN_ROUTE } from '../utils/consts';
-
+//База данныех firebase
 import { Context } from '../firebase';
 import firebase from 'firebase/compat/app';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { setDoc, doc, Timestamp } from 'firebase/firestore';
-
+//Тема
 const theme = createTheme({
   palette: {
     mode: 'dark',
@@ -27,14 +28,18 @@ const theme = createTheme({
 });
 
 const SignUp = () => {
+  // получаю данные через контекст
   const { auth, firestore } = useContext(Context);
 
+  const history = useNavigate();
+
   const signUp = async () => {
+    //Авторизация по google через popup
     const provider = new firebase.auth.GoogleAuthProvider();
     const { user } = await auth.signInWithPopup(provider);
     console.log(user);
   };
-
+  // данные для регистрации
   const [data, setData] = useState({
     name: '',
     lastName: '',
@@ -43,30 +48,38 @@ const SignUp = () => {
     error: null,
     loading: false,
   });
+  // достаю из даты данные через деструктуризацию
   const { name, lastName, email, password, error, loading } = data;
+  // делаю инпут управляемым
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
-  console.log(firestore);
+  // асинхронная функция при нажатии на кнопку
   const handleSubmit = async (e) => {
     e.preventDefault();
     setData({ ...data, error: null, loading: true });
+    // валидация на пустоту
     if (!name || !lastName || !email || !password) {
       setData({ ...data, error: 'Заполнить все поля' });
     }
-    try {
-      const result = await createUserWithEmailAndPassword(auth, email, password);
 
+    try {
+      // регистрация по почте
+      const result = await createUserWithEmailAndPassword(auth, email, password);
+      // создаю колекцию в дб
       await setDoc(doc(firestore, 'users', result.user.uid), {
         uid: result.user.uid,
         name,
+        password,
         email,
         createdAt: Timestamp.fromDate(new Date()),
         isOnline: true,
       });
+      // очещаю поля
       setData({ name: '', email: '', password: '', error: null, loading: false });
-      // firebase.firestore().collection('users').doc().set({});
+      history('/');
     } catch (error) {
+      // вывожу ошибку
       setData({ ...data, error: error.message, loading: false });
     }
   };
@@ -136,16 +149,29 @@ const SignUp = () => {
                 />
               </Grid>
             </Grid>
+            {/* вывод ошибки */}
             {error ? (
               <Typography align="center" color="error">
                 {error}
               </Typography>
             ) : null}
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-              Зарегистрироваться
+            <Button
+              disabled={loading}
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              {loading ? 'Загрузка...' : 'Зарегистрироваться'}
             </Button>
-            <Button onClick={signUp} fullWidth variant="contained" sx={{ mb: 2 }}>
-              Войти через Google
+            <Button
+              disabled={loading}
+              onClick={signUp}
+              fullWidth
+              variant="contained"
+              sx={{ mb: 2 }}
+            >
+              {loading ? 'Загрузка...' : 'Войти через Google'}
               <GoogleIcon />
             </Button>
             <Grid container justifyContent="flex-end">
